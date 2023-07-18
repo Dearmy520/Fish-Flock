0ffc8593317718c049b778ce2b4f9cdd39e86163
Dearmy520 <jiangzehao157@gmail.com>
2023-07-18 13:50:38 +0800

    Unity

---
+++ Assets/Script/CohesionBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Cohesion")]
+public class CohesionBehavior : FlockBehavior
+{
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+
+
+        if (context.Count == 0) return Vector2.zero;
+
+        Vector2 cohensionMove = Vector2.zero;
+
+        foreach (Transform item in context)
+        {
+            cohensionMove += (Vector2)item.position;
+        }
+
+        cohensionMove /= context.Count;
+
+        cohensionMove -= (Vector2)agent.transform.position;
+        return cohensionMove;
+    }
+}
---
+++ Assets/Script/SteerCohesion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Steer")]
+public class SteerCohesion : FlockBehavior
+{
+    Vector2 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+
+
+        if (context.Count == 0) return Vector2.zero;
+
+        Vector2 cohensionMove = Vector2.zero;
+
+        foreach (Transform item in context)
+        {
+            cohensionMove += (Vector2)item.position;
+        }
+
+        cohensionMove /= context.Count;
+
+        cohensionMove -= (Vector2)agent.transform.position;
+        cohensionMove = Vector2.SmoothDamp(agent.transform.up, cohensionMove, ref currentVelocity, agentSmoothTime);
+        return cohensionMove;
+    }
+}
